e902fef9f9387e19fff156e35ef8778b03cd814a
royaljackab <jackabribat@gmail.com>
2026-02-18 16:18:53 +0100

    Ajout dans pool + commentaires

--- lib/ecs/pool.h
+++ lib/ecs/pool.h
@@ -1,11 +1,25 @@
+/***************************************************************
+ * @file pool.h
+ * @brief Structure contenant l'ECS
+ *
+ * UTILISATION :
+ *  1) Créer les fichiers.h et .c décrivant votre composante
+ *  2) Dans le .h, utiliser les macros DEFINE_COMPONENT_MANAGER
+ *  et DECLARE_SETTER_GETTER dans ecs/component.h
+ *  3) Rajouter votre ComponentManger ici, dans la structure Pool
+ *  4) Ajouter la fonction Component_init dans Pool_init (ecs/pool.c)
+ *  5) Ajouter la fonction Component_remove dans pool_kill_entity (ecs/pool.c)
+ *
+ ***************************************************************/
+
 #pragma once
 
 #include "components/collision_circle.h"
 #include "components/collision_rectangle.h"
 #include "components/common.h"
+#include "components/life.h"
 #include "components/physics.h"
 #include "components/sprite.h"
-#include "ecs/component.h"
 #include "ecs/ecs.h"
 
 typedef struct Pool {
@@ -15,6 +29,7 @@ typedef struct Pool {
   SpriteManager sprite;
   Collision_circleManager collision_circle;
   Collision_rectangleManager collision_rectangle;
+  LifeManager life;
 
   // Nouvelle gestion des indices
   Entity free_indices[MAX_ENTITIES]; // Le tableau qui stocke les IDs dispos
