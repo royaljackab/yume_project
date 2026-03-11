424a05a81692c403b2da3563c362df54a5ce1f77
Samuel <samuelklein019@gmail.com>
2026-03-11 12:11:55 +0100

    Particle, collision

--- src/components/bullet.c
+++ src/components/bullet.c
@@ -6,29 +6,21 @@
 
 Entity Bullet_enemy_spawn(Pool *p, float x, float y, float speed, float angle,
                     SpriteID graphic) {
-  
-  Entity e = pool_create_entity(p);
-
-  Position pos = {{x, y}, angle};
-  Physics phy = Physics_create_speed(speed);
-  Tag tag = ENT_ENEMY_SHOT;
-
-  Position_add(&p->position, e, pos);
-  Physics_add(&p->physics, e, phy);
-  Sprite_add(&p->sprite, e, sprites[graphic]);
-  Tag_add(&p->tag, e, tag);
-
-  return e;
+  Bullet_spawn(p, x, y, speed, angle, ENT_ENEMY, graphic);
 }
 
 Entity Bullet_player_spawn(Pool *p, float x, float y, float speed, float angle,
+                    SpriteID graphic) {
+  Bullet_spawn(p, x, y, speed, angle, ENT_PLAYER_SHOT, graphic);
+}
+
+Entity Bullet_spawn(Pool *p, float x, float y, float speed, float angle, Tag tag, 
                     SpriteID graphic) {
   
   Entity e = pool_create_entity(p);
 
   Position pos = {{x, y}, angle};
   Physics phy = Physics_create_speed(speed);
-  Tag tag = ENT_PLAYER_SHOT;
 
   Position_add(&p->position, e, pos);
   Physics_add(&p->physics, e, phy);
---
+++ src/components/collision.c
@@ -0,0 +1,39 @@
+
+#include "components/common.h"
+#include "components/collision_rectangle.h"
+#include "ecs/pool.h"
+#include "player.h"
+#include <raylib.h>
+
+
+
+
+void Entity_is_hit(Pool *p, Entity entity);
+
+void Player_is_hit_by_bullet(Pool *p, Player player) {
+  /**
+   * Verifie si le joueur et touchés par un projectile (circulaire) ennemis
+   */
+  
+  Player player = p->player.dense[0];
+
+
+  SpriteManager *collision_circle_manager = &p->collision_circle;
+  PositionManager *positionManager = &p->position;
+
+
+  Position pos;
+  Vector2 * playerPos = Player_get_position(p, player);
+  Collision_circle * collision;
+  float playerRadius = Collision_circle_get_radius(Player_get_collision(p, player));
+  int lookup;
+
+  for (int i = 0; i < collision_circle_manager->count; i++) {
+    collision = &collision_circle_manager->dense[i];
+    lookup = collision_circle_manager->entity_lookup[i];
+    pos = positionManager->dense[lookup];
+    if (&pos != Player_get_position(playerPos)){
+        if (CheckCollisionCircles(*playerPos, playerRadius, Position_get_pos(&pos), Collision_circle_get_radius(&collision)));
+            DrawText("TU ES TOUCH2S MON DIEU 9A MARCHe!!!!!", 70, 30, 50, RED);
+    }
+}
