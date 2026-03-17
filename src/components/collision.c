0be5586d86984fa0b7f0de4cc6195e10daa8c3f8
Samuel <samuelklein019@gmail.com>
2026-03-17 09:40:48 +0100

    Fix collision.c et drawAll()

--- src/components/collision.c
+++ src/components/collision.c
@@ -22,7 +22,7 @@ void Player_is_hit_by_bullet(Pool *p, Player player) {
   PositionManager *positionManager = &p->position;
 
 
-  Position pos;
+  Position * pos;
   Vector2 * playerPos = Player_get_position(p, player);
   Collision_circle * collision;
   float playerRadius = Collision_circle_get_radius(Player_get_collision(p, player));
@@ -31,9 +31,10 @@ void Player_is_hit_by_bullet(Pool *p, Player player) {
   for (int i = 0; i < collision_circle_manager->count; i++) {
     collision = &collision_circle_manager->dense[i];
     lookup = collision_circle_manager->entity_lookup[i];
-    pos = positionManager->dense[lookup];
-    if (&pos != Player_get_position(playerPos)){
-        if (CheckCollisionCircles(*playerPos, playerRadius, Position_get_pos(&pos), Collision_circle_get_radius(&collision)));
+    pos = Position_get(positionManager, lookup);
+
+    if (&pos != Player_get_position(p, player)){
+        if (CheckCollisionCircles(*playerPos, playerRadius, Position_get_pos(pos), Collision_circle_get_radius(&collision)));
             DrawText("TU ES TOUCH2S MON DIEU 9A MARCHe!!!!!", 70, 30, 50, RED);
     }
 }
