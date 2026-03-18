184e0972252a8bb6f58aa6b48dcd9041887f3db7
Samuel <samuelklein019@gmail.com>
2026-03-18 16:33:23 +0100

    debut enemy.c

---
+++ lib/components/enemy.h
@@ -0,0 +1,2 @@
+Entity Enemy_spawn(Pool *p, float x, float y, float speed, float angle, int healthPoints
+                    SpriteID graphic);
---
+++ src/components/enemy.c
@@ -0,0 +1,23 @@
+
+#include "components/bullet.h"
+#include "common.h"
+#include "physics.h"
+#include "pool.h"
+#include "sprite.h"
+#include "enemy.h"
+
+Entity Enemy_spawn(Pool *p, float x, float y, float speed, float angle, int healthPoints,
+                    SpriteID graphic) {
+  
+  Entity e = pool_create_entity(p);
+
+  Position pos = {{x, y}, angle};
+  Physics phy = Physics_create_speed(speed);
+  Life life = {.life = healthPoints, .maxLife = healthPoints };
+  Position_add(&p->position, e, pos);
+  Physics_add(&p->physics, e, phy);
+  Sprite_add(&p->sprite, e, sprites[graphic]);
+  Tag_add(&p->tag, e, ENT_ENEMY);
+  Life_add(&p->life, e, life);
+  return e;
+}
