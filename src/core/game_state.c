cc7dad7d0aa04b1c414599c94f74f31a6a3edcab
Amy El Alem <amyelalem@gmail.com>
2026-04-08 03:37:36 +0200

    ajout de l'état game over (state_game_over.h/c) et détection de la mort du joueur dans moonlight

--- src/core/game_state.c
+++ src/core/game_state.c
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include "content/ui/state_menu_settings.h"
+#include "content/ui/state_game_over.h"
 
 // Variable statique pour savoir ce qu'on modifie en pause
 // 0 = BGM, 1 = SFX
@@ -27,6 +28,8 @@ GameState *get_state_pointer(StateID state) {
     return &state_menu_settings;
   case STATE_MOONLIGHT:
     return &state_moonlight;
+  case STATE_GAME_OVER:
+    return &state_game_over;
   
   default:
     return 0;
