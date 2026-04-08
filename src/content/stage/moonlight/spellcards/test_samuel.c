53ed03d6fabb7d9310a9827c4a36befc70215784
Azyor <samuelklein19@outlook.fr>
2026-04-08 10:09:38 +0200

    création branche pattern samuel

---
+++ src/content/stage/moonlight/spellcards/test_samuel.c
@@ -0,0 +1,21 @@
+#include "spellcards/brouwer_fixed_point.h"
+#include "assets.h"
+#include "bullet.h"
+#include "ecs.h"
+#include "systems/obj.h"
+#include "screen.h"
+#include "tasks.h"
+#include "common.h"
+#include <raylib.h>
+
+#include "stdio.h"
+
+
+TASK(moving_ring,{Pool * pool; float spawn_x; float spawn_y; float radius; float angle; float param_angvel; float param_treshold; float param_burst}){
+    while (true)
+    {
+        /* code */
+    }
+    
+
+}
